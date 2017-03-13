b531d7b9359802e956f3f6511f6b215fa189c69b
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-03-13 17:04:46 -0400

    Check that we are finding all other required filetypes

--- spec/traversal.rb
+++ spec/traversal.rb
@@ -1,4 +1,5 @@
 require "./lib/traversal"
+require "./lib/helpers"
 
 RSpec.describe Traversal do
   before(:all) do
@@ -6,7 +7,39 @@ RSpec.describe Traversal do
     @traversal.traverse
   end
 
+  it "tests the filetypes we require" do
+    expect(FILETYPES.include?('htm')).to be true
+    expect(FILETYPES.include?('html')).to be true
+    expect(FILETYPES.include?('css')).to be true
+    expect(FILETYPES.include?('js')).to be true
+    expect(FILETYPES.include?('pdf')).to be true
+    expect(FILETYPES.include?('xml')).to be true
+    expect(FILETYPES.length).to eq(6)
+  end
+
   it "finds all the .htm files" do
     expect(@traversal.count('htm')).to eq(10)
   end
+
+  it "finds all the .html files" do
+    # (there aren't actually any)
+    expect(@traversal.count('html')).to eq(0)
+  end
+
+  it "finds all the .css files" do
+    expect(@traversal.count('css')).to eq(13)
+  end
+
+  it "finds all the .js files" do
+    expect(@traversal.count('js')).to eq(9)
+  end
+
+  it "finds all the .pdf files" do
+    expect(@traversal.count('pdf')).to eq(24)
+  end
+
+  it "finds all the .xml files" do
+    expect(@traversal.count('xml')).to eq(32)
+  end
+
 end
