91c6c92acfcd8efea38cdb40953c35cdcbd24e89
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-03-15 08:52:43 -0400

    Start to test our parser (which will find and rewrite http links).

---
+++ spec/parser.rb
@@ -0,0 +1,18 @@
+require "./lib/parser"
+require "./lib/helpers"
+
+RSpec.describe Parser do
+  it "finds all the http://ocw.mit.edu links in xml" do
+    @parser = Parser.new
+    testfile = File.join(File.dirname(__FILE__), '../files/75804/imsmanifest.xml')
+    @parser.update([testfile])
+    expect(@parser.processed).to eq(6)
+  end
+
+  it "finds all the http://ocw.mit.edu links in html" do
+    @parser = Parser.new
+    testfile = File.join(File.dirname(__FILE__), '../files/75804/5-301-january-iap-2004/contents/index.htm')
+    @parser.update([testfile])
+    expect(@parser.processed).to eq(76)
+  end
+end
