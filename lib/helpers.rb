73e15b816307a1ff65ccaa2ad11c7a98bff7220b
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-03-30 15:32:32 -0400

    Update filetypes
    .pdfs are binary junk, but filenames without extensions are where the
    js actually lives, and that's what we usually care about.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,5 +1,6 @@
 # The filetypes that the program will examine for OCW http links.
-FILETYPES = ['htm', 'html', 'pdf', 'xml', 'css', 'js']
+# Javascript is often in files without extensions, so we include ''.
+FILETYPES = ['htm', 'html', 'xml', 'css', 'js', 'txt', '']
 
 # Where the packager command lives.
 DSPACE_BIN_DIR = '/home/dspace/dspace.mit.edu/bin/dspace'
