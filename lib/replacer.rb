84ca68042f66d27b970b14d76250c63383672d3e
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-04-04 14:18:47 -0400

    Also change YouTube and Internet Archive links
    Some media aren't hosted at OCW, so they are subject to the same mixed-
    content failures as http OCW links. We can, however, reasonably trust
    that YouTube and IA will be available via https, so let's change them
    too.

--- lib/replacer.rb
+++ lib/replacer.rb
@@ -29,29 +29,27 @@ module Replacer
           begin
             temp_file = Tempfile.new('closed_courseware')
             @@current_filename = filename
-            regex = /http:\/\/(ocw[0-9]*.mit.edu)/
 
             File.open(filename, 'r+') do |file|
               file.each_line do |line|
                 begin
-                  @links_processed += line.gsub(regex).count
-                  subbed_line = line.gsub!(regex, 'https://\1')
+                  line = https_it(line)
 
-                  # gsub returns nil if it doesn't make any substitutions. In that
-                  # case, we want to use the original line.
-                  temp_file.puts subbed_line ? subbed_line : line
+                  temp_file.puts line
                 rescue
                   @errors = true
                 end
               end
             end
 
-            temp_file.close
             FileUtils.mv(temp_file.path, filename)
+            temp_file.close
+            temp_file.unlink
+
             if !@errors
               @files_processed[filename] = true
             end
-          rescue
+          ensure
             temp_file.close
             temp_file.unlink
           end
@@ -81,5 +79,23 @@ module Replacer
       @links_processed = 0
       @files_processed = {}
     end
+
+    private
+      def https_it(line)
+        # Although the scope of this project is making OCW audio/video
+        # links work, some of the links failing due to mixed content are
+        # in fact calling out to YouTube or Internet Archive. They can be
+        # trusted to have https, though, so let's do that.
+        regexes = [/http:\/\/(ocw[0-9]*.mit.edu)/,
+                   /http:\/\/([a-zA-Z0-9_]*.archive.org)/,
+                   /http:\/\/([a-zA-Z0-9_]*.youtube.com)/]
+
+        line = line.encode('UTF-8', :invalid => :replace)
+        regexes.each do |regex|
+          @links_processed += line.gsub(regex).count
+          line.gsub!(regex, 'https://\1')
+        end
+        line
+      end
   end
 end
